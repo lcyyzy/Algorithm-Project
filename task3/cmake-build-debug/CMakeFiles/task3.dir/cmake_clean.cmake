46c53f6dfa05255bad119655c299e5434698efa9
lcyyzy <14307130318@fudan.edu.cn>
2017-06-25 16:03:18 +0800

    submit version

---
+++ task3/cmake-build-debug/CMakeFiles/task3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task3.dir/main.cpp.o"
+  "task3.pdb"
+  "task3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
